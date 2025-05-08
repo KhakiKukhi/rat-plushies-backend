496a98b8a19707c2528fa222fdbaa9092aa40adf
Koung <k.koungmyathein@student.maastrichtuniversity.nl>
2025-05-08 22:29:01 +0200

    Initial Commit

---
+++ src/main/java/lol/khakikukhi/ratplushiesbackend/RatPlushiesBackendApplication.java
@@ -0,0 +1,13 @@
+package lol.khakikukhi.ratplushiesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatPlushiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatPlushiesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lol/khakikukhi/ratplushiesbackend/RatPlushiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lol.khakikukhi.ratplushiesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatPlushiesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
